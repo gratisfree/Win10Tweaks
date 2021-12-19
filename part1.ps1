c8e33c1f2d3972432af7fcf2dcf5d6f60161e2df
Marius S <96331860+gratisfree@users.noreply.github.com>
2021-12-19 08:55:44 +0000

    Update part1.ps1

--- part1.ps1
+++ part1.ps1
@@ -1,4 +1,4 @@
- Write-Host "Disabling Telemetry..."
+ Write-Host "Disabling Telemetry"
     Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
     Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
     Disable-ScheduledTask -TaskName "Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" | Out-Null
